e8a0bf88421afbfc5c309d4503b0f6d989476470
David Kumarbai <cygnidavid@gmail.com>
2019-11-16 12:36:36 -0800

    add create event route

--- routes/api.js
+++ routes/api.js
@@ -1,38 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const Hangout = require("./models/Events");
+const Hangout = require("../models/Events");
 
-router.post('/create_event', (req, res) => {
-  const { body } = req;
-  const {
-    title,
-    time,
-    loc,
-    description,
-    creatorName,
-    creatorId,
-  } = body;
-  const hangout = new Hangout({
-    title,
-    time,
-    loc,
-    description,
-    creatorName,
-    creatorId,
-  });
-  hangout.save((err) => {
-    if (err) new Error(err);
-    res.sendStatus(200);
-  })
-})
-// TODO
-// router.get('/dashboard/all')
-// TODO
-// router.get('/dashboard/going')
-// TODO
-// router.get('/dashboard/past')
-
-/* GET home page. */
 router.get("/", function(req, res, next) {
   res.send({
     test: 123,
@@ -41,5 +10,28 @@ router.get("/", function(req, res, next) {
     anArray: [1, 2, 3, 4, 5]
   });
 });
+
+router.post('/create_event', (req, res) => {
+  const {
+    title,
+    time,
+    loc,
+    description,
+    creator_name,
+    creator_id,
+  } = body;
+  const hangout = new Hangout({
+    title,
+    time,
+    loc,
+    description,
+    creator_name,
+    creator_id,
+  });
+  hangout.save((err) => {
+    if (err) throw new Error(err);
+    res.sendStatus(200);
+  })
+})
 
 module.exports = router;
