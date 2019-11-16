989610db63a1d3b1d62594c86c7e9a4f12f4ede0
Andrew Patterson <apat24@mail.sfsu.edu>
2019-11-16 12:32:38 -0800

    Adds user model.

--- app.js
+++ app.js
@@ -15,6 +15,39 @@ mongoose.set('useCreateIndex', true);
 
 app.use(express.json());
 app.use(bodyParser.json())
+require("./models/Events");
+require("./models/Users");
+
+const User = mongoose.model("users");
+
+const newUser = new User({
+  facebook_id: "34tfdcnvk",
+  first_name: "Steve",
+  last_name: "Jobs",
+  email: "blsd@gfd.com",
+  interests: ["weed", "pizza", "beer"]
+});
+
+newUser.save(err => {
+  console.log(err);
+});
+
+// const Event = mongoose.model("events");
+
+// const newEvent = new Event({
+//   title: "Another",
+//   time: moment().format(),
+//   location: {
+//     lat_long: "234,1234",
+//     coordinates: [123, 123]
+//   },
+//   creator_id: "fgdnkvc123",
+//   guest_ids: ["gfdgdf", "dfgidjfnv"]
+// });
+
+// newEvent.save(err => {
+//   console.log(err);
+// });
 
 app.use("/", indexRouter);
 app.use("/api", apiRouter);
---
+++ models/Users.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const Schema = mongoose.Schema;
+
+const UserSchema = Schema({
+  facebook_id: {
+    type: String,
+    required: true
+  },
+  first_name: {
+    type: String,
+    required: true
+  },
+  last_name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    validate: val => {
+      if (!validator.isEmail(val)) {
+        throw new Error("Invalid email address");
+      }
+    }
+  },
+  age: {
+    type: Number
+  },
+  gender: {
+    type: String
+  },
+  description: {
+    type: String
+  },
+  interests: [
+    {
+      type: String
+    }
+  ],
+  rating: {
+    type: Number,
+    min: 0,
+    max: 5
+  }
+});
+
+mongoose.model("users", UserSchema);
