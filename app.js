9b80f7ac60f47beb71fd5edd4a277b46836f85c4
David Kumarbai <cygnidavid@gmail.com>
2019-11-16 12:35:45 -0800

    app change

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
 require("dotenv").config();
 
 const express = require("express");
-const apiRoutes = require("./routes/api");
+const indexRouter = require("./routes/index");
+const apiRouter = require("./routes/api");
 const mongoose = require("mongoose");
 const app = express();
+
+const bodyParser = require("body-parser");
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
 app.use(express.json());
+app.use(bodyParser.json())
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// Routes
-app.use("/api", apiRoutes);
+app.use("/api", apiRouter);
+// app.use('/users', usersRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
