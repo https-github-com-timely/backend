b2a8e2d14a14b839ca3c60ecfe7fb238ae259494
David Kumarbai <cygnidavid@gmail.com>
2019-11-16 12:38:47 -0800

    double quotes

--- app.js
+++ app.js
@@ -13,9 +13,9 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 app.use(express.json());
 app.use(bodyParser.json())
 
-app.use('/', indexRouter);
+app.use("/", indexRouter);
 app.use("/api", apiRouter);
-// app.use('/users', usersRouter);
+// app.use("/users", usersRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
