1622bc100d3761a1e9013042353627c5c848e41f
Andrew Patterson <apat24@mail.sfsu.edu>
2019-11-16 15:15:56 -0800

    adds google-geocoder

--- services/distance.js
+++ services/distance.js
@@ -1,9 +1,36 @@
 const geolib = require("geolib");
+const NodeGeocoder = require("node-geocoder");
 
-module.exports.getDistance = (coords1, coords2) => {
+const options = {
+  provider: "google",
+  // Optional depending on the providers
+  httpAdapter: "https", // Default
+  apiKey: process.env.GOOGLE_API_KEY, // for Mapquest, OpenCage, Google Premier
+  formatter: null // 'gpx', 'string', ...
+};
+
+const geocoder = NodeGeocoder(options);
+
+const getLatLongFromAddress = address => {
+  geocoder
+    .geocode(address)
+    .then(function(res) {
+      console.log(res);
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+};
+
+const getDistance = (coords1, coords2) => {
   const distanceInMeters = geolib.getDistance(coords1, coords2);
   const distanceInMi = geolib.convertDistance(distanceInMeters, "mi");
   return distanceInMi;
+};
+
+module.exports.geoFunctions = {
+  getDistance,
+  getLatLongFromAddress
 };
 
 // Usage:
