7be3786d16cd33ef73a91e4a4551b9f310103023
Andrew Patterson <apat24@mail.sfsu.edu>
2019-11-16 10:04:46 -0800

    Adds dependencies

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
+// Environment variables
+require("dotenv").config();
+
+// Dependencies
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
+// Routes
 var indexRouter = require("./routes/index");
 var apiRouter = require("./routes/api");
 
 var app = express();
 
+// Mongoose
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+require("./models/Events");
+
+console.log(process.env.MONGO_URI);
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
