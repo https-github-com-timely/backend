5cec44b89ed4ac55207c46c26e9b88ffe860ec70
Andrew Patterson <apat24@mail.sfsu.edu>
2019-11-16 12:32:38 -0800

    Adds user model.

--- app.js
+++ app.js
@@ -10,6 +10,38 @@ app.use(express.json());
 const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 require("./models/Events");
+require("./models/Users");
+
+const User = mongoose.model("users");
+
+const newUser = new User({
+  facebook_id: "34tfdcnvk",
+  first_name: "Steve",
+  last_name: "Jobs",
+  email: "blsd@gfd.com",
+  interests: ["weed", "pizza", "beer"]
+});
+
+newUser.save(err => {
+  console.log(err);
+});
+
+// const Event = mongoose.model("events");
+
+// const newEvent = new Event({
+//   title: "Another",
+//   time: moment().format(),
+//   location: {
+//     lat_long: "234,1234",
+//     coordinates: [123, 123]
+//   },
+//   creator_id: "fgdnkvc123",
+//   guest_ids: ["gfdgdf", "dfgidjfnv"]
+// });
+
+// newEvent.save(err => {
+//   console.log(err);
+// });
 
 // Routes
 app.use("/api", apiRoutes);
